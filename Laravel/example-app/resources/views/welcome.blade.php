326dfa34c9bf904e98eb05d9a8e65d80e8bbacdd
Mithursan Kalanantharasan <mitho9798@gmail.com>
2024-08-02 08:39:17 +0530

    02-08-2024

--- Laravel/example-app/resources/views/welcome.blade.php
+++ Laravel/example-app/resources/views/welcome.blade.php
@@ -1,3 +1,23 @@
 <x-layout>
     <p>Welcome Home Page</p>
+    @foreach ($data2 as $m)
+        {{$m}}
+    @endforeach
+
+    @foreach ($data2 as $m => $n)
+    {{$m}}-{{$n}}
+    @endforeach
+
+    <table border="1px ">
+    @foreach ($data2 as $m => $n)
+   
+        <tr>
+            <td>{{$m}}</td>
+            <td>{{$n}}</td>
+        </tr>
+    
+    @endforeach
+</table>
+    
+
 </x-layout>
--- Laravel/example-app/routes/web.php
+++ Laravel/example-app/routes/web.php
@@ -1,16 +1,43 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+/*
+Route::get('/', function () {
+    // return ("Mithursan");
+    // $marks = [
+    //     "Tamil" => 25,
+    //     "English" => 75,
+    //     "Science" => 35
+    // ];
+    // return $marks;
+
+    return view('welcome')
+});
+*/
+
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $name = "Mithursan";
+    $marks = [
+        "Tamil" => 25,
+        "English" => 75,
+        "Science" => 35
+    ];
+
+    return view('welcome', [
+        'data1' => $name,
+        'data2' => $marks
+    ]);
 });
+
 
 /*
 Route::get('/{page?}', function ($page = 'index') {
     return view("pages/" . $page);
 });
 */
+
 
 Route::get('/photo', function () {
     return view('pages.photo');
