b902eb043b686bc750d873785e6e8da0b86f4983
Mithursan Kalanantharasan <mitho9798@gmail.com>
2024-08-07 21:05:44 +0530

    07-08-2024

--- Laravel/example-app/resources/js/bootstrap.js
+++ Laravel/example-app/resources/js/bootstrap.js
@@ -1,4 +1,5 @@
-import axios from 'axios';
+import axios from "axios";
+import bootstarp from "bootstrap";
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
--- Laravel/example-app/resources/views/components/layout.blade.php
+++ Laravel/example-app/resources/views/components/layout.blade.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href= "{{asset('css/style.css')}}" rel="stylesheet"/>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <table border="1" width="100%">
@@ -35,6 +34,7 @@
             <td colspan="2">Footer</td>
         </tr>
     </table>
+
 </body>
 
 </html>
